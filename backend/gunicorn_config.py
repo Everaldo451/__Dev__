eda7d647104dd927150d8c0d710673333e190e00
Everaldo451 <everaldo.work.jr@gmail.com>
2025-01-20 15:13:32 -0300

    using wsl

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 bind = f"{os.getenv("HOST")}:{os.getenv("PORT")}"
 workers = 2
--- backend/run.py
+++ backend/run.py
@@ -1,11 +1,3 @@
 from app.app import create_app
-from dotenv import load_dotenv
-import os
 
-if __name__ == "main":
-    load_dotenv()
-    
-    HOST = os.getenv("HOST")
-    PORT = os.getenv("PORT")
-
-    app = create_app()
+app = create_app()
